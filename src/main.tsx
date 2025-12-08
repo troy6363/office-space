423c8bfe8f2fdb1661d36f2715a8a2d02332c837
troy6363 <tjbuys4cash@gmail.com>
2025-12-07 18:46:59 -0600

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import emailjs from '@emailjs/browser';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
 import './index.css';
-
-emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <App />
   </StrictMode>
 );
